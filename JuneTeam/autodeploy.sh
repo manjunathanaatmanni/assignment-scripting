7e43b162569d3d3592b486b5e96b754351a7c143
manjunathanaatmanni <107828947+manjunathanaatmanni@users.noreply.github.com>
2022-07-08 16:20:58 +0530

    Update autodeploy.sh

--- JuneTeam/autodeploy.sh
+++ JuneTeam/autodeploy.sh
@@ -3,7 +3,7 @@ mvn clean install
 
 #mvn deploy
 
-cp /var/lib/jenkins/workspace/manjupipeline/JuneTeam/target*.war /home/ubuntu/repos/assignment-scripting/Newpackages/apache-tomcat/webapps
+sudo cp /var/lib/jenkins/workspace/manjupipeline/JuneTeam/target*.war /home/ubuntu/repos/assignment-scripting/Newpackages/apache-tomcat/webapps
 
 cd /home/ubuntu/repos/assignment-scripting/Newpackages/apache-tomcat/bin
 bash startup.sh
